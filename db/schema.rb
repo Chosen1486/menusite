d6fbbdcb9f06d3c4933374a4b5ecc21f7b602990
Chosen <qq4868@hotmail.com>
2018-12-12 16:28:46 +0800

    create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_032302) do
+ActiveRecord::Schema.define(version: 2018_12_12_074142) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,11 +33,18 @@ ActiveRecord::Schema.define(version: 2018_12_12_032302) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "tel"
     t.string "address"
     t.text "description"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
