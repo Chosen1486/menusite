60095eae6d6da9822332f1c724b6563c2b6df4f5
Chosen <qq4868@hotmail.com>
2018-12-16 00:05:15 +0800

    add show page for public users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,12 @@
 class RestaurantsController < ApplicationController
   include Pagy::Backend
-  
+
   def index
     @pagy, @restaurants = pagy(Restaurant.all, items: 20)
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
