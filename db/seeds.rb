3394519a5c248d1028f1523c20445b3df2506faf
Chosen <qq4868@hotmail.com>
2018-12-16 10:15:13 +0800

    remove district seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,30 +21,12 @@ category_list = [
   { name: "其他" }
 ]
 
-district_list =[
-  { name: "中正區" },
-  { name: "大同區" },
-  { name: "中山區" },
-  { name: "松山區" },
-  { name: "大安區" },
-  { name: "萬華區" },
-  { name: "信義區" },
-  { name: "士林區" },
-  { name: "北投區" },
-  { name: "內湖區" },
-  { name: "南港區" },
-  { name: "文山區" },
-]
 
 category_list.each do |category|
   Category.create( name: category[:name] )
 end
 puts "Category created!"
 
-district_list.each do |district|
-  District.create( name: district[:name] )
-end
-puts "Category created!"
 
 User.create(email: "test@example.com", password: "testtest", role: "admin")
 puts "Default admin created!"
