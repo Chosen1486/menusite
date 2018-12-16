53007ed16e314cf72d38db2f9845ec65b8d83d18
Chosen <qq4868@hotmail.com>
2018-12-16 13:31:35 +0800

    use enum to refract authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def admin?
-    self.role == "admin"
+  enum role: {user:0, vip:1, admin:2}
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
   end
   
 end
