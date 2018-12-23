1b6b9f337a3efb51d6841fb7542987a5d1c14a7c
Chosen <qq4868@hotmail.com>
2018-12-23 16:33:05 +0800

    add default user role setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
   
   enum role: {user:0, vip:1, admin:2}
 
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
